87ebbd63fa605e359d03ed49fd36d6224e28ee71
mkhembrom <manjeshembrom@gmail.com>
2021-08-14 23:58:15 +0530

    first commit

---
+++ src/main/java/com/manjesh/OnlineStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.manjesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreProjectApplication.class, args);
+	}
+
+}
